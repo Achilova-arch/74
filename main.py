e7577faa5a5f99dc70ce7ba7ce47b1cd81a1d155
Achilova-arch <asalachilova96@gmail.com>
2025-09-16 09:01:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+text="Asal"
+text2="Achilova"
+print( text.upper())
+print( text2.upper())
+
+text3="axdcvb nmmjjnnhgfdsxggcvb dfghnjm fvgbhnm fghjk dfghjdfghjfghjvdfghj"
+print(len(text3))
+
+text4="Листья сбрасывает сад, В воздухе прохлада. Ветки любят листопад, Отдахнуть им надо"
+print(text.find('сад'))
+
+text5="Листья сбрасывает сад, В воздухе прохлада. Ветки любят листопад, Отдахнуть им надо"
+print(text5.strip())
+
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
